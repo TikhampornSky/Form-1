6b3915f11584ea8980e25a04f4834f08eb5ecc6d
TikhampornSky <tikhamporntan@gmail.com>
2022-09-01 15:19:04 +0700

    Send

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,28 @@
+class MainController < ApplicationController
+  def test
+    #redirect_to '/main/test2'
+  end
+
+  def test2
+    @num1 = params[:score1].to_i
+    @num2 = params[:score2].to_i
+    @num3 = params[:score3].to_i
+    @s1 = params[:subject1]
+    @s2 = params[:subject2]
+    @s3 = params[:subject3]
+    @summ = @num1 + @num2 + @num3
+
+    @max_score = [@num1, @num2, @num3].max
+    @max_subject = ''
+    if (@max_score === @num1)
+      @max_subject = @s1
+    elsif (@max_score === @num2)
+      @max_subject = @s2
+    elsif (@max_score === @num3)
+      @max_subject = @s3
+    else 
+      @max_subject = ''
+    end
+  end
+
+end
